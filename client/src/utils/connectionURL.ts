18c9409feb7b6479f2a0007d0493080468cb937b
James <118572805+JamesCorcion@users.noreply.github.com>
2023-04-10 14:46:39 -0400

    reconnected to deployment

--- client/src/utils/connectionURL.ts
+++ client/src/utils/connectionURL.ts
@@ -1,2 +1,2 @@
-//export const baseURL = 'https://pinterestclone.onrender.com/api/'
-export const baseURL = 'http://localhost:8080/api/'
+export const baseURL = 'https://pinterestclone.onrender.com/api/'
+//export const baseURL = 'http://localhost:8080/api/'
