cf421cefeeb469209ed98c98a1d2a21b327b55f1
James <118572805+JamesCorcion@users.noreply.github.com>
2023-04-22 15:27:25 -0400

    multiple updates to docker and url

--- client/src/utils/connectionURL.ts
+++ client/src/utils/connectionURL.ts
@@ -1,3 +1,3 @@
 //export const baseURL = 'https://pinterestclone.onrender.com/api/'
-export const baseURL = 'https://master.pinterestbackend.c66.me/'
-//export const baseURL = 'http://localhost:8080/api/'
+//export const baseURL = 'https://master.pinterestbackend.c66.me/'
+export const baseURL = 'http://localhost:8080/api/'
