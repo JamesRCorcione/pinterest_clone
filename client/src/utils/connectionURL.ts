3de64590e34224d228f121537978099088be2611
James <118572805+JamesCorcion@users.noreply.github.com>
2023-04-23 12:16:24 -0400

    changed backend url

--- client/src/utils/connectionURL.ts
+++ client/src/utils/connectionURL.ts
@@ -1,3 +1,3 @@
 //export const baseURL = 'https://pinterestclone.onrender.com/api/'
-export const baseURL = '34.145.190.4:8080'
+export const baseURL = 'https://server-t33wok22pa-uk.a.run.app/'
 //export const baseURL = 'http://localhost:8080/api/'
