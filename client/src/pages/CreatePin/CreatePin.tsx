a2b8aa1fc770de64092dc3209e5b386eda06a6a5
James <118572805+JamesCorcion@users.noreply.github.com>
2023-04-26 14:31:16 -0400

    custom image drag and drop implemented

--- client/src/pages/CreatePin/CreatePin.tsx
+++ client/src/pages/CreatePin/CreatePin.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { createPin } from '../../features/pinsSlice'
 import FileBase from 'react-file-base64'
+import { FileUploader } from "react-drag-drop-files"
 import { grey } from '@mui/material/colors'
 import useStyles from './styles'
 
@@ -13,9 +14,11 @@ interface CreatePinProps {
     }
 }
 
+
 const CreatePin = ({user}:CreatePinProps) => {
-  const [tag, setTag] = useState<string>('')
+    const [tag, setTag] = useState<string>('')
     const [tags, setTags] = useState<string[]>([])
+    const [dragActive, setDragActive] = useState<boolean>(false);
     const [pin, setPin] = useState({ title: '', text: '', tags: tags, creatorId: user?.result._id, totalComments: 0, postedBy: {userId: user?.result._id,  userName: user?.result.userName, image: null}, image: '', destination: '' })    
     const [chips, setChips] = useState<string[]>([])
     const navigate = useNavigate()
@@ -40,9 +43,7 @@ const CreatePin = ({user}:CreatePinProps) => {
       } catch (error:any) {
         alert(error.message)
       }
-    }
-
-
+    }    
 
     const handleDeleteTag = (chipDelete:string) => {
       const updatedChips = chips.filter((chip) => chip !== chipDelete)
@@ -58,11 +59,76 @@ const CreatePin = ({user}:CreatePinProps) => {
         alert('Please Enter a New Tag!')
       }
       e.target.reset()
+
     }
+
+    const handlePhotoUpload = async (e:any) => {
+      let file = e.target.files[0]
+      let reader = new FileReader()
+
+      if (file) {
+        reader.readAsDataURL(file)
+        reader.onload = function(){
+          let fileInfo = {
+            name: file.name,
+            type: file.type,
+            size: Math.round(file.size / 1000) + ' kB',
+            base64: reader.result,
+            file: file,
+          }
+
+          if (fileInfo.base64)
+            setPin({ ...pin, image: fileInfo.base64.toString() })
+          return fileInfo
+        }
+      }
+    }
+
+    const handleDrag = (e:any) => {
+      e.preventDefault()
+      e.stopPropagation()
+      if (e.type === "dragenter" || e.type === "dragover") {
+        setDragActive(true);
+      } else if (e.type === "dragleave") {
+        setDragActive(false);
+      }
+    }
+
+    const handleDrop = function(e:any) {
+      e.preventDefault();
+      e.stopPropagation();
+      setDragActive(false);
+      if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+        // at least one file has been dropped so do something
+        // handleFiles(e.dataTransfer.files);
+        let file = e.dataTransfer.files[0]
+        let reader = new FileReader()
+        
+        if (file) {
+          reader.readAsDataURL(file)
+          reader.onload = function(){
+            let fileInfo = {
+              name: file.name,
+              type: file.type,
+              size: Math.round(file.size / 1000) + ' kB',
+              base64: reader.result,
+              file: file,
+            }
+  
+            if (fileInfo.base64)
+              setPin({ ...pin, image: fileInfo.base64.toString() })
+            return fileInfo
+          }
+        }
+
+      
+      }
+    };
 
   return (
     <Box className={classes.background}>
-      <Box className={classes.createPinContainer} >
+      <Box className={classes.createPinContainer}>
+
         <form onSubmit={handlePinSave}>
           <input
             type="text"
@@ -101,8 +167,10 @@ const CreatePin = ({user}:CreatePinProps) => {
             </div>
           </div>
 
-          <div><FileBase type="file" multiple={false} onDone={({ base64 }:any) => setPin({ ...pin, image: base64 })} /></div>
-      
+
+         
+
+
           <Button
             type="submit"
             variant="contained"
@@ -116,29 +184,76 @@ const CreatePin = ({user}:CreatePinProps) => {
           </Button>
           
         </form>  
+
+        <form onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+            <label title='Photo Upload'>
+              <Box 
+                  onDragEnter={handleDrag}
+                  onDragLeave={handleDrag} 
+                  onDragOver={handleDrag} 
+                  onDrop={handleDrop}
+                  draggable
+                  sx={{display: 'flex',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    flexDirection: 'row',
+                    width: '20vw',
+                    padding: 5,
+                    borderStyle: 'solid',
+                    borderWidth: 1,
+                    borderColor: 'green',
+                    borderRadius: 5,
+                    color: 'green',
+                  }}>
+                    Drag and Drop to Upload Photo
+                <Input 
+                  type="file"  
+                  disableUnderline
+                  sx={{display: 'none'}}
+                  onChange={(e) => handlePhotoUpload(e)}
+                />
+              </Box>            
+            </label>
+            { dragActive && 
+            
+              <Box 
+                id="drag-file-element" 
+                onDragEnter={handleDrag}
+                onDragLeave={handleDrag} 
+                onDragOver={handleDrag} 
+                onDrop={handleDrop}>
+              </Box> 
+            }
+        </form>
         
+          {pin?.image &&
+          <Box sx={{position: 'absolute', top: 100, right: 180}}>
+            <img src={pin?.image} width={'350px'} border-radius={10}></img>
+          </Box>
+          }
+
         <Box>
-          <form onSubmit={(e) => handleAddTag(e)} >
-            <Input 
-              sx={{backgroundColor: 'white'}}
-              name="tags" 
-              fullWidth 
-              value={tag} 
-              onChange={(e:any) => setTag(e.target.value)}
-            />  
-          </form>
+        <form onSubmit={handleAddTag}>
+          <Input                  
+            onChange={(e:any) => setTag(e.target.value)}
+            placeholder='Add some tags!'
+            disableUnderline={true}
+            sx={{border: 1, marginLeft: 1}}
+            />
           <Button
-              onClick={handleAddTag}
-              type="submit"
-              variant="contained"
-              size="small"
-              sx={{
-                marginLeft: 10,
-                fontFamily: "'Abel', 'sansSerif'",
-              }}
-            >
-                Add Tag       
-            </Button>
+            type="submit"
+            variant="contained"
+            size="small"
+            sx={{
+              marginLeft: 10,
+              fontFamily: "'Abel', 'sansSerif'",
+            }}
+          >
+              Add Tag       
+          </Button>
+          
+        </form>
+          
           </Box>
           {chips &&
             chips?.map((chip:any, i:number) => (
