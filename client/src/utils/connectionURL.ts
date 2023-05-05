45225fb36545d5bf5bd036bb741d3a2406352bc0
James <118572805+JamesCorcion@users.noreply.github.com>
2023-05-05 15:21:29 -0400

    updated backend connection and build

--- client/src/utils/connectionURL.ts
+++ client/src/utils/connectionURL.ts
@@ -1,3 +1,4 @@
+export const baseURL = 'https://server-2-t33wok22pa-uk.a.run.app/api/'
 //export const baseURL = 'https://pinterestclone.onrender.com/api/'
-export const baseURL = 'https://updatedserver-t33wok22pa-uk.a.run.app/api/'
+//export const baseURL = 'https://updatedserver-t33wok22pa-uk.a.run.app/api/'
 //export const baseURL = 'http://localhost:8080/api/'
